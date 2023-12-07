8b7f81f965ddba9a5d0cb972fb60255efb288a08
brian <90752841+wokbjso@users.noreply.github.com>
2023-12-07 23:35:26 +0900

    Feat:헤더 라우터에 전역 선언

---
+++ src/common/layout/Header.tsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export default function Header() {
+  return <HeaderLayout>Header</HeaderLayout>;
+}
+
+const HeaderLayout = styled.header`
+  font-size: 2rem;
+`;
