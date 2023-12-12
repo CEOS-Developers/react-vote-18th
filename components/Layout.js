b12fc10f1249a80001c1fd14a35d9800943ddf0f
leejin_rho <tbdpapdl@gmail.com>
2023-12-13 03:26:56 +0900

    style: loginBox, 이미지 위치 조정

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
 export default function Layout({ children }) {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <>
-      <Image src="/images/ceosIcon.svg" width={200} height={80} />
-      {/* "CEOS 7주차 과제: REDDI 팀" */}
+      <div className={styles.headBox}>
+        <Image src="/images/ceosIcon.svg" width={172} height={73} />
+        {isLogin ? null : (
+          <div className={styles.loginBox}>
+            <Link href="/login">
+              <button className={`${styles.buttons} ${styles.loginButton}`}>
+                로그인
+              </button>
+            </Link>
+            <Link href="/signup">
+              <button className={`${styles.buttons} ${styles.signupButton}`}>
+                회원가입
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+
       <div>{children}</div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import HeadFunction from "../components/HeadFunction";
+import Image from "next/image";
 //예시 기본 홈페이지 -> 처음 접속하면 로그인이나 회원가입 받게하기
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <HeadFunction title="Home" />
-      <h1>기본 Home page</h1>
-      <Link href="/login">
-        <button className={styles.buttons}>로그인하기</button>
-      </Link>
-      <Link href="/signup">
-        <button className={styles.buttons}>회원가입하기</button>
-      </Link>
+      <h1>파트장 / 데모데이 투표</h1>
     </div>
   );
 }
