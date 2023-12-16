d82041801c5059a3e903f6240ad216f575647517
leejin_rho <tbdpapdl@gmail.com>
2023-12-17 02:29:27 +0900

    style: team, part 기본 스타일 구현

--- pages/vote/part.js
+++ pages/vote/part.js
@@ -1,11 +1,62 @@
 // /vote/part 페이지
+import { useState } from "react";
 import HeadFunction from "../../components/HeadFunction";
+import styles from "../../styles/Part.module.css";
 
 export default function votePart() {
+  const [isFront, setIsFront] = useState(true);
+
+  const peopleList = [
+    {
+      id: 1,
+      team: "REDDI",
+      name: "노이진",
+    },
+    {
+      id: 2,
+      team: "REDDI",
+      name: "신동현",
+    },
+    {
+      id: 3,
+      team: "gotcha",
+      name: "변지혜",
+    },
+    {
+      id: 4,
+      team: "SNIFF",
+      name: "송지석",
+    },
+    {
+      id: 5,
+      team: "SNIFF",
+      name: "오대균",
+    },
+    {
+      id: 6,
+      team: "셰어마인드",
+      name: "이규호",
+    },
+  ];
+
   return (
-    <>
+    <div className={styles.partContainer}>
       <HeadFunction title="파트장 투표" />
-      vote Part
-    </>
+      <h1 className={styles.title}>
+        {isFront ? "FE 파트장 투표" : "BE 파트장 투표"}
+      </h1>
+      <div className={styles.partList}>
+        {peopleList.map((list) => (
+          <button className={styles.partBox}>
+            <div className={styles.teamName}>{list.team}</div>
+            <div className={styles.name}>{list.name}</div>
+          </button>
+        ))}
+      </div>
+      <div style={{ display: "flex", marginTop: 101 }}>
+        <button className={styles.voteButton}>투표하기</button>
+        <button className={styles.resultButton}>결과보기</button>
+      </div>
+    </div>
   );
 }
--- pages/vote/team.js
+++ pages/vote/team.js
@@ -1,12 +1,70 @@
 // /vote/team 페이지
 
+import { useState } from "react";
 import HeadFunction from "../../components/HeadFunction";
+import styles from "../../styles/Team.module.css";
 
+const peopleList = [
+  {
+    id: 1,
+    name: "REDDI",
+    exp: "주식 관리 포트폴리오 서비스",
+  },
+  {
+    id: 2,
+    name: "gotcha",
+    exp: "주식 관리 포트폴리오 서비스",
+  },
+  {
+    id: 3,
+    name: "SNIFF",
+    exp: "주식 관리 포트폴리오 서비스",
+  },
+  {
+    id: 4,
+    name: "셰어마인드",
+    exp: "주식 관리 포트폴리오 서비스",
+  },
+  {
+    id: 5,
+    name: "로컬무드",
+    exp: "주식 관리 포트폴리오 서비스",
+  },
+];
 export default function VoteTeam() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <>
-      <HeadFunction title="팀 투표" />
-      vote team
-    </>
+    <div className={styles.teamContainer}>
+      <HeadFunction title="데모데이 투표" />
+      <h1 className={styles.title}>데모데이 투표</h1>
+      <div className={styles.partList}>
+        {peopleList.map((list) => (
+          <button
+            className={`${styles.teamBox} ${isClicked ? styles.clicked : ""}`}
+            onClick={() => setIsClicked(!isClicked)}
+          >
+            <div
+              className={`${styles.teamName} ${
+                isClicked ? styles.clickedText : ""
+              }`}
+            >
+              {list.name}
+            </div>
+            <div
+              className={`${styles.teamExp} ${
+                isClicked ? styles.clickedText : ""
+              }`}
+            >
+              {list.exp}
+            </div>
+          </button>
+        ))}
+      </div>
+      <div style={{ display: "flex", marginTop: 117 }}>
+        <button className={styles.voteButton}>투표하기</button>
+        <button className={styles.resultButton}>결과보기</button>
+      </div>
+    </div>
   );
 }
