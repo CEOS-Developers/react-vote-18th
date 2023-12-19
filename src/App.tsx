ddf5316fb73ed13ec58ea07069bbe7fb8a96b597
silver <ekdldkaa@naver.com>
2023-12-19 17:12:27 +0900

    [feat] 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './style/theme';
+import Main from './pages/main';
+import Signup from './pages/signup';
+import Login from './pages/login';
+import TeamVote from './pages/teamvote';
+import MemberVote from './pages/membervote';
 
 function App() {
-  return <div className="App">냐냐냥~</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/teamvote" element={<TeamVote />} />
+          <Route path="/membervote" element={<MemberVote />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/style/theme.tsx
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    key: '#FD505B',
+    sub: '#FFA5AA',
+    black: '#292525',
+    white: '#FBFBFB',
+    gray: '#989292',
+    lightgray: '#ECEAEA',
+    kakao: '#FBE93D',
+  },
+};
