bfbc87f991493900b7da953ed8b02a521bdcfd0a
brian <90752841+wokbjso@users.noreply.github.com>
2023-12-21 00:56:13 +0900

    Feat:Auth HOC 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./common/layout/Header/Header";
 import VoteCategory from "./pages/vote/VoteCategory";
 import VotePartLeader from "./pages/vote/VotePartSelect";
@@ -8,21 +13,32 @@ import Login from "./pages/auth/Login";
 import VoteLeader from "./pages/vote/VoteLeader";
 import VoteResults from "./pages/vote/VoteResults";
 import VoteDemoday from "./pages/vote/VoteDemoday";
+import { ProtectedRoute } from "./features/auth/components/ProtectedRoute/ProtectedRoute";
 
 function App() {
+  const user = false; //로그인 로직
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<VoteCategory />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/select-part" element={<VotePartLeader />} />
-        <Route path="/select-demoday" element={<VoteDemodaySelect />} />
-        <Route path="/select-leader" element={<VoteLeader />} />
-        <Route path="/vote-leader" element={<VoteLeader />} />
-        <Route path="/vote-demoday" element={<VoteDemoday />} />
-        <Route path="/vote-results" element={<VoteResults />} />
+        <Route
+          element={<ProtectedRoute isAllowed={!user} redirectPath={"/"} />}
+        >
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        <Route
+          element={<ProtectedRoute isAllowed={user} redirectPath={"/login"} />}
+        >
+          <Route path="/select-part" element={<VotePartLeader />} />
+          <Route path="/select-demoday" element={<VoteDemodaySelect />} />
+          <Route path="/select-leader" element={<VoteLeader />} />
+          <Route path="/vote-leader" element={<VoteLeader />} />
+          <Route path="/vote-demoday" element={<VoteDemoday />} />
+          <Route path="/vote-results" element={<VoteResults />} />
+        </Route>
+        <Route path={"/*"} element={<Navigate to={"/"} />} />
       </Routes>
     </Router>
   );
