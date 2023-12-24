1981a5937413e27af71e97036c7512017ff3567a
brian <90752841+wokbjso@users.noreply.github.com>
2023-12-24 15:34:52 +0900

    토큰 처리 임의 로직 생성

--- src/features/auth/queries/dto/auth-login.ts
+++ src/features/auth/queries/dto/auth-login.ts
@@ -2,3 +2,12 @@ export interface AuthLoginRequest {
   email: string;
   password: string;
 }
+
+export interface AuthLoginResponse {
+  code: number;
+  code_desc: string;
+  data: {
+    accessToken: string;
+    refreshToken: string;
+  };
+}
