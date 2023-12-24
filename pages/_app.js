40d2e8f589368cbebe0ee6c6af52985a3f1e38fb
leejin_rho <tbdpapdl@gmail.com>
2023-12-25 04:08:03 +0900

    feat: project result get api 연결

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { RecoilRoot } from "recoil";
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryCache,
+} from "@tanstack/react-query";
+import { useState } from "react";
+
 export default function App({ Component, pageProps }) {
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error) => toast.error(`Something went wrong: ${error.message}`),
+    }),
+  });
+
   return (
-    <RecoilRoot>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
