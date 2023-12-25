adf37ffc4390d7b6286fc8d379dfc873d917ae99
김지원 <jibom0507@naver.com>
2023-12-26 01:15:10 +0900

    Feat: 데모데이 투표 결과보기 dto와query 생성 및 rank 적용

--- src/pages/vote/VoteResults.tsx
+++ src/pages/vote/VoteResults.tsx
@@ -6,6 +6,8 @@ import MediaQuery from "@/styles/mediaQuery";
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import useGetTeam from "@/features/vote/queries/useGetTeam";
 
 const VoteResults = () => {
   const { isMobile } = MediaQuery();
@@ -13,38 +15,19 @@ const VoteResults = () => {
   const navigateHome = () => {
     navigate("/");
   };
-  const demodayResults = [
-    {
-      mainText: "LocalMood",
-      subText: "키워드로 찾는 나만의 장소",
-      rank: 1,
-      voteNum: 5,
-    },
-    {
-      mainText: "GOTCHA",
-      subText: "면접자 관리 및 면접 지원 서비스",
-      rank: 2,
-      voteNum: 4,
-    },
-    {
-      mainText: "SNIFF",
-      subText: "향수 입문자를 위한 서비스",
-      rank: 3,
-      voteNum: 4,
-    },
-    {
-      mainText: "레디",
-      subText: "레퍼런스 아카이빙 구독 서비스",
-      rank: 4,
-      voteNum: 2,
-    },
-    {
-      mainText: "셰어마인드",
-      subText: "연애상담 마켓플레이스",
-      rank: 5,
-      voteNum: 0,
-    },
-  ]; //API 연결
+
+  const [teamData, setTeamData] = useState([]);
+  useEffect(() => {
+    useGetTeam("/app/team")
+      .then((teamData) => {
+        teamData = teamData.data;
+        setTeamData(teamData);
+      })
+      .catch((error) => {
+        console.error("팀 데이터를 불러오는 중 에러 발생:", error);
+      });
+  }, []);
+
   const leaders = [
     { mainText: "김현민", subText: "LocalMood", rank: 1, voteNum: 5 },
     { mainText: "김지원", subText: "LocalMood", rank: 2, voteNum: 5 },
@@ -63,7 +46,8 @@ const VoteResults = () => {
   const part = location.state.part;
   const pageHeaderText =
     type === "leader" ? `${part} 파트장 투표 결과` : "데모데이 투표 결과";
-  const results = type === "leader" ? leaders : demodayResults;
+  const results = type === "leader" ? leaders : teamData;
+  const sortedResults = results.sort((a, b) => b.voteCnt - a.voteCnt);
   const voteType =
     type === "leader" ? SELECT_TYPE.PartLeader : SELECT_TYPE.Demoday;
 
@@ -71,14 +55,14 @@ const VoteResults = () => {
     <VoteResultsContainer>
       <PageMainText text={pageHeaderText} />
       <ResultsContainer $isMobile={isMobile}>
-        {results.map((result, index) => (
+        {sortedResults.map((result, index) => (
           <VoteResult
             key={index}
             type={voteType}
-            mainText={result.mainText}
-            subText={result.subText}
-            rank={result.rank}
-            voteNum={result.voteNum}
+            mainText={result.name}
+            subText={result.description}
+            rank={index + 1}
+            voteNum={result.voteCnt}
           />
         ))}
       </ResultsContainer>
