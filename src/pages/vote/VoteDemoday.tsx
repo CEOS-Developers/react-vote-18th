603915877e4ba1fc9b053a4431b2e6e0139e9c28
김지원 <jibom0507@naver.com>
2023-12-26 02:33:03 +0900

    VoteDemoday dummylist 삭제

--- src/pages/vote/VoteDemoday.tsx
+++ src/pages/vote/VoteDemoday.tsx
@@ -12,14 +12,6 @@ import usePatchTeamVote from "@/features/vote/queries/usePatchTeamVote";
 const VoteDemoday = () => {
   const { isMobile } = MediaQuery();
   const navigate = useNavigate();
-  const demodays = [
-    { mainText: "LocalMood", subText: "키워드로 찾는 나만의 장소" },
-    { mainText: "GOTCHA", subText: "면접자 관리 및 면접 지원 서비스" },
-    { mainText: "SNIFF", subText: "향수 입문자를 위한 서비스" },
-    { mainText: "레디", subText: "레퍼런스 아카이빙 구독 서비스" },
-    { mainText: "셰어마인드", subText: "연애상담 마켓플레이스" },
-  ]; //API 연결
-
   const [data, setData] = useState([]);
   useEffect(() => {
     useGetResults("/app/team")
