0a733f434d23f819a1cd99d7ead1d8332df27d2b
김지원 <jibom0507@naver.com>
2023-12-26 01:52:03 +0900

    Feat: 파트장 투표 결과보기 API 연결

---
+++ src/features/vote/queries/dto/vote-response.ts
@@ -0,0 +1,19 @@
+export interface VoteResponse {
+  code: number;
+  code_desc: string;
+  data: VoteTeamDataItem[] | VoteMemberDataItem;
+}
+
+export interface VoteTeamDataItem {
+  id: number;
+  name: string;
+  description: string;
+  voteCnt: number;
+}
+
+export interface VoteMemberDataItem {
+  id: number;
+  name: string;
+  teamName: string;
+  voteCnt: number;
+}
--- src/pages/vote/VoteResults.tsx
+++ src/pages/vote/VoteResults.tsx
@@ -7,7 +7,7 @@ import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import useGetTeam from "@/features/vote/queries/useGetTeam";
+import useGetResults from "@/features/vote/queries/useGetResults";
 
 const VoteResults = () => {
   const { isMobile } = MediaQuery();
@@ -16,38 +16,32 @@ const VoteResults = () => {
     navigate("/");
   };
 
-  const [teamData, setTeamData] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    useGetTeam("/app/team")
-      .then((teamData) => {
-        teamData = teamData.data;
-        setTeamData(teamData);
+    let url;
+    if (type !== "leader") {
+      url = "/app/team";
+    } else {
+      const devPartId = part === "BE" ? 1 : 2;
+      url = `/app/member?devPartId=${devPartId}`;
+    }
+
+    useGetResults(url)
+      .then((resultData) => {
+        const data = resultData.data;
+        setData(data);
       })
       .catch((error) => {
-        console.error("팀 데이터를 불러오는 중 에러 발생:", error);
+        console.error("데이터를 불러오는 중 에러 발생:", error);
       });
   }, []);
-
-  const leaders = [
-    { mainText: "김현민", subText: "LocalMood", rank: 1, voteNum: 5 },
-    { mainText: "김지원", subText: "LocalMood", rank: 2, voteNum: 5 },
-    { mainText: "노이진", subText: "Reddigg", rank: 3, voteNum: 5 },
-    { mainText: "신동현", subText: "Reddigg", rank: 4, voteNum: 5 },
-    { mainText: "정인영", subText: "셰어마인드", rank: 5, voteNum: 5 },
-    { mainText: "이규호", subText: "셰어마인드", rank: 6, voteNum: 5 },
-    { mainText: "변지혜", subText: "Gotcha", rank: 7, voteNum: 5 },
-    { mainText: "이은비", subText: "Gotcha", rank: 8, voteNum: 5 },
-    { mainText: "오대균", subText: "Sniff", rank: 9, voteNum: 5 },
-    { mainText: "송지석", subText: "Sniff", rank: 10, voteNum: 5 },
-  ]; //API 연결
 
   const location = useLocation();
   const type = location.state.type;
   const part = location.state.part;
   const pageHeaderText =
     type === "leader" ? `${part} 파트장 투표 결과` : "데모데이 투표 결과";
-  const results = type === "leader" ? leaders : teamData;
-  const sortedResults = results.sort((a, b) => b.voteCnt - a.voteCnt);
+  const sortedResults = data.sort((a, b) => b.voteCnt - a.voteCnt);
   const voteType =
     type === "leader" ? SELECT_TYPE.PartLeader : SELECT_TYPE.Demoday;
 
